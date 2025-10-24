21819445fc005f03b9ef0efccf93319c7969f759
LagomaxK <lagomaxravenna+kart@gmail.com>
2025-10-24 18:54:52 +0200

    Modify opening hours in orari.js
    Updated opening hours for various days of the week.

--- orari.js
+++ orari.js
@@ -1,10 +1,11 @@
 lun = 'Chiuso';
-mar = '15:00 - 19:00 Aperto su prenotazione';
-mer = '15:00 - 19:00 Aperto su prenotazione';
-gio = '15:00 - 20:00';
-ven = '15:00 - 20:00';
-sab = '09:30 - 12:00 | 15:00 - 20:00';
-dom = '09:30 - 12:00 | 15:00 - 20:00';
+mar = '15:00 - 18:00 Aperto su prenotazione minimo quattro persone';
+mer = '15:00 - 19:00 Aperto su prenotazione minimo quattro persone';
+gio = '15:00 - 18:00';
+ven = '15:00 - 18:00';
+sab = '09:30 - 12:00 | 15:00 - 18:00';
+dom = '09:30 - 12:00 | 15:00 - 18:00';
+
 
 
 
