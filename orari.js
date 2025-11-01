7e05d2ac6001b99c2b58dc436adddb5bc5d2d01a
LagomaxK <lagomaxravenna+kart@gmail.com>
2025-11-01 09:11:14 +0100

    Update opening hours for weekdays

--- orari.js
+++ orari.js
@@ -1,10 +1,11 @@
 lun = 'Chiuso';
-mar = '15:00 - 17:00 Aperto su prenotazione minimo quattro persone';
-mer = '15:00 - 17:00 Aperto su prenotazione minimo quattro persone';
-gio = '15:00 - 17:00 Aperto su prenotazione minimo quattro persone';
-ven = '15:00 - 17:00 Aperto su prenotazione minimo quattro persone';
+mar = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
+mer = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
+gio = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
+ven = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
 sab = '09:30 - 12:00 | 14:00 - 17:00';
 dom = '09:30 - 12:00 | 14:00 - 17:00';
+
 
 
 
