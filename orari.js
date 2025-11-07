80c3b212cf48c62b7ac4b89d02cbe4222fc2ed6a
LagomaxK <lagomaxravenna+kart@gmail.com>
2025-11-07 08:43:27 +0100

    Modify Sunday hours in orari.js
    Updated Sunday hours to reflect closure due to marathon.

--- orari.js
+++ orari.js
@@ -4,7 +4,8 @@ mer = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
 gio = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
 ven = '14:30 - 17:00 Aperto su prenotazione minimo quattro persone';
 sab = '09:30 - 12:00 | 14:00 - 17:00';
-dom = '09:30 - 12:00 | 14:00 - 17:00';
+dom = 'Questa mattina Chiuso (causa maratona Ravenna) riapriamo dalle | 14:00 - 17:00';
+
 
 
 
