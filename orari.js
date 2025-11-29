54713144caf5ac2a7856749b2db6677671d01b99
LagomaxK <lagomaxravenna+kart@gmail.com>
2025-11-29 09:07:59 +0100

    Change Saturday and Sunday hours to 'Chiuso riapriamo a gennaio'
    Updated opening hours for Saturday and Sunday.

--- orari.js
+++ orari.js
@@ -3,8 +3,9 @@ mar = 'Chiuso';
 mer = 'Chiuso';
 gio = 'Chiuso';
 ven = 'Chiuso';
-sab = '09:30 - 12:00 | 14:00 - 17:00';
-dom = '09:30 - 12:00 | 14:00 - 17:00';
+sab = 'Chiuso riapriamo a gennaio';
+dom = 'Chiuso riapriamo a gennaio';
+
 
 
 
